74fc42266d8eab9fc23deaa4c506a6ebc7e08e3a
DanisAvko <valievdanis@bk.ru>
2019-10-13 20:57:32 +0300

    lst

--- src/Main.kt
+++ src/Main.kt
@@ -18,13 +18,14 @@ fun main() {
     val l6 = Newton(arrayOf(-1.0, 0.0, 1.0, 2.0), arrayOf(0.0, 1.0, 2.0, 9.0))
     println(l6)
     val ls=Newton(arrayOf(), arrayOf());
-    println(ls);
     ls.addNode(0.0,0.0)
-    println(ls);
     ls.addNode(1.0,1.0)
     println(ls);
     ls.addNode(-1.0,1.0)
     println(ls);
-
+    val l7 = Newton(arrayOf(-1.5, -0.75, 0.0, 0.75,1.5), arrayOf(-14.1014, -0.931596, 0.0, 0.931596,14.1014))
+    println(l7)
+    val l8 = Lagrange(arrayOf(-1.5, -0.75, 0.0, 0.75,1.5), arrayOf(-14.1014, -0.931596, 0.0, 0.931596,14.1014))
+    println(l8)
 
 }
